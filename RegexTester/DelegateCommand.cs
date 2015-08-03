0039ce35b97395964b9d316e6ce2db39e3167205
349550050 <pilimt@126.com>
2015-08-03 08:28:46 +0800

    update namespace

--- RegexTester/DelegateCommand.cs
+++ RegexTester/DelegateCommand.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace V2.Elements.Paramers
+namespace RegexTester
 {
     public class DelegateCommand<T>:CommandBase
     {
--- RegexTester/MainpageViewModel.cs
+++ RegexTester/MainpageViewModel.cs
@@ -7,7 +7,6 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using RegexTester;
-using V2.Elements.Paramers;
 
 namespace RegexTester
 {
